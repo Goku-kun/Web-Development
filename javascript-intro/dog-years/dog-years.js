69ed21e329fbf6dd6bf48cd67fd1ca96eac11f12
Goku-kun <jethvadharmarajsinh@gmail.com>
2020-09-29 20:57:27 +0530

    human to dog year convertor

---
+++ javascript-intro/dog-years/dog-years.js
@@ -0,0 +1,11 @@
+let myAge = 21;
+// declared my age
+let earlyYears = 2;
+// declared earlyyears
+earlyYears *= 10.5;
+laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears + ' ' + laterYears);
+myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Goku kun'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
