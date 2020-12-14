f32d5397fc19e8ce628e296c1997cc548182f536
Goku-kun <jethvadharmarajsinh@gmail.com>
2020-12-14 22:06:52 +0530

    Final update

--- instruments-javascript/resources/javascript/script.js
+++ instruments-javascript/resources/javascript/script.js
@@ -7,6 +7,14 @@ function playSound(e) {
     key.classList.add('playing');
 }
 
+function playSoundPointer(e) {
+    const audio = document.querySelector(`audio[data-key="${e.path[1].dataset['key']}"]`)
+    const key = document.querySelector(`.key[data-key="${e.path[1].dataset['key']}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
@@ -15,4 +23,4 @@ function removeTransition(e) {
 const keys = document.querySelectorAll('.key')
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 document.addEventListener('keydown', playSound);
-document.addEventListener('pointerup', playSound);
+document.addEventListener('pointerup', playSoundPointer);
